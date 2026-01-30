de107f5c1afe10060510ddbd443e842312ca5027
zihadulislam2 <zihadul708@gmail.com>
2026-01-31 01:53:06 +0600

    refactor(auth): improve error handling and update role checks in middleware
    refactor(user.router): update user routes to use OWNER and EMPLOYER roles
    
    refactor(user.constant): ensure USER_ROLE constants are correctly defined

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,50 +1,50 @@
-import { Router } from "express";
-import userController from "./user.controller";
-import validateRequest from "../../middleware/validateRequest";
-import { userValidation } from "./user.validation";
-import auth from "../../middleware/auth";
-import { USER_ROLE } from "./user.constant";
-import { upload } from "../../middleware/multer.middleware";
+import { Router } from 'express'
+import userController from './user.controller'
+import validateRequest from '../../middleware/validateRequest'
+import { userValidation } from './user.validation'
+import auth from '../../middleware/auth'
+import { USER_ROLE } from './user.constant'
+import { upload } from '../../middleware/multer.middleware'
 
-const router = Router();
+const router = Router()
 
 router.post(
-  "/register",
+  '/register',
   validateRequest(userValidation.userValidationSchema),
-  userController.registerUser
-);
+  userController.registerUser,
+)
 
 router.post(
-  "/verify-email",
-  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
-  userController.verifyEmail
-);
+  '/verify-email',
+  auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+  userController.verifyEmail,
+)
 
 router.post(
-  "/resend-otp",
-  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
-  userController.resendOtpCode
-);
+  '/resend-otp',
+  auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+  userController.resendOtpCode,
+)
 
-router.get("/all-users", userController.getAllUsers);
+router.get('/all-users', userController.getAllUsers)
 router.get(
-  "/my-profile",
-  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
-  userController.getMyProfile
-);
+  '/my-profile',
+  auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+  userController.getMyProfile,
+)
 
 router.put(
-  "/update-profile",
-  upload.single("image"),
-  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
-  userController.updateUserProfile
-);
+  '/update-profile',
+  upload.single('image'),
+  auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+  userController.updateUserProfile,
+)
 
 router.get(
-  "/admin_id",
-  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
-  userController.getAdminId
-);
+  '/admin_id',
+  auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+  userController.getAdminId,
+)
 
-const userRouter = router;
-export default userRouter;
+const userRouter = router
+export default userRouter
