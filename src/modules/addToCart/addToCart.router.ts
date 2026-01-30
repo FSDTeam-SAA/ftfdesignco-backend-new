0677dfe7730e5551529a12821c048499f62ccb04
Nsarkar-xlr8 <knayemlol24@gmail.com>
2026-01-31 02:55:24 +0600

    added the authguard in the add to cart modules

--- src/modules/addToCart/addToCart.router.ts
+++ src/modules/addToCart/addToCart.router.ts
@@ -1,12 +1,34 @@
 import { Router } from 'express'
 import addToCartController from './addToCart.controller'
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = Router()
 
-router.post('/', addToCartController.addToCart)
-router.get('/:userId', addToCartController.getCartByUserId)
-router.put('/:id', addToCartController.updateCart)
-router.delete('/:id', addToCartController.removeFromCart)
-router.delete('/clear/:userId', addToCartController.clearCart)
+router.post(
+    '/',
+    auth(USER_ROLE.EMPLOYER),
+    addToCartController.addToCart);
+
+router.get(
+    '/:userId',
+    auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+    addToCartController.getCartByUserId);
+
+
+router.put(
+    '/:id',
+    auth(USER_ROLE.EMPLOYER),
+    addToCartController.updateCart);
+
+router.delete(
+    '/:id',
+    auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+    addToCartController.removeFromCart);
+
+router.delete(
+    '/clear/:userId',
+    auth(USER_ROLE.OWNER, USER_ROLE.EMPLOYER),
+    addToCartController.clearCart)
 
 export const addToCartRouter = router
