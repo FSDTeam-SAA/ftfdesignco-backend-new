26ea2525719d65d5cc12535555c3e42c74c46736
Nsarkar-xlr8 <knayemlol24@gmail.com>
2026-02-01 23:11:51 +0600

    corsOptions added

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,33 @@ import notFound from "./middleware/notFound";
 import { applySecurity } from "./middleware/security";
 import router from "./router";
 import httpLogger from "./httpLogger";
+import cors from 'cors';
 
 const app: Application = express();
 
 // applySecurity(app);
+
+
+const allowedOrigins = [
+  'https://pjf-clothing.vercel.app/',
+  'http:'//localhost:3000',
+
+]
+
+const corsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Required if you are sending cookies or authorization headers
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+};
+
+app.use(cors(corsOptions));
 
 app.use(httpLogger);
 
