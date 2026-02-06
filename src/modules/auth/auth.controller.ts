4ed75781d6f3eeaa99e7adb0935cf6b2177339cc
Sajjad177 <sajjadhossainx06@gmail.com>
2026-02-07 02:20:32 +0600

    code removed

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,11 @@
 import { StatusCodes } from "http-status-codes";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
-import config from "../../config";
 import authService from "./auth.service";
 
 const login = catchAsync(async (req, res) => {
   const result = await authService.login(req.body);
-  // const { refreshToken, accessToken, user } = result;
 
-  // res.cookie("refreshToken", refreshToken, {
-  //   httpOnly: true,
-  //   secure: config.NODE_ENV === "production",
-  //   sameSite: config.NODE_ENV === "production" ? "none" : "lax",
-  //   maxAge: 7 * 24 * 60 * 60 * 1000,
-  // });
 
   sendResponse(res, {
     statusCode: StatusCodes.OK,
