d1886838808310dc62372d24ab7d828f6761ecaf
zihadulislam2 <zihadul708@gmail.com>
2026-02-19 03:03:10 +0600

    refactor: clean up order router code and improve formatting

--- src/modules/order/order.router.ts
+++ src/modules/order/order.router.ts
@@ -1,52 +1,42 @@
 import { Router } from 'express'
 import orderController from './order.controller'
-import auth from '../../middleware/auth';
-import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middleware/auth'
+import { USER_ROLE } from '../user/user.constant'
 
 const router = Router()
 
 router.post(
-    '/',
+  '/',
 
-    orderController.createOrder);
-
-
-router.get(
-    '/get-all',
-    auth(USER_ROLE.OWNER),
-    orderController.getAllOrders);
-
+  orderController.createOrder,
+)
 
 router.get(
-    '/:id',
-    auth(USER_ROLE.OWNER),
-    orderController.getOrderById);
+  '/get-all',
+  // auth(USER_ROLE.OWNER),
+  orderController.getAllOrders,
+)
 
+router.get('/:id', auth(USER_ROLE.OWNER), orderController.getOrderById)
 
 router.get(
-    '/user/:userId',
-    auth(USER_ROLE.EMPLOYER),
-    orderController.getOrdersByUserId);
+  '/user/:userId',
+  auth(USER_ROLE.EMPLOYER),
+  orderController.getOrdersByUserId,
+)
 
-
-router.put(
-    '/:id',
-    orderController.updateOrderStatus);
-
+router.put('/:id', orderController.updateOrderStatus)
 
 router.delete(
-    '/:id',
-    auth(USER_ROLE.OWNER),
-    orderController.deleteOrder);
-
+  '/:id',
+  auth(USER_ROLE.OWNER, USER_ROLE.ADMIN),
+  orderController.deleteOrder,
+)
 
 router.get(
-    '/user/my-history',
-    auth(USER_ROLE.EMPLOYER),
-    orderController.getMyHistory
-);
-
-
-
+  '/user/my-history',
+  auth(USER_ROLE.EMPLOYER),
+  orderController.getMyHistory,
+)
 
 export const orderRouter = router
